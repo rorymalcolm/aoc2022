14919f4168ede6d03f7447ea3beb15608a5268af
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2022-12-01 10:53:02 +0000

    fix

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,8 +1,7 @@
 use std::{
-    collections::{BTreeMap, HashMap},
+    collections::HashMap,
     fs::File,
     io::{self, BufRead},
-    iter::Map,
     path::Path,
 };
 
@@ -44,8 +43,6 @@ fn main() {
     println!("Max value: {}", max_val);
 }
 
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
